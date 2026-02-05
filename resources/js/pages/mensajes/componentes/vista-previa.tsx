be005c67d32a457ce962b69385b2ab98869a17dc
Daniel Acevedo <danielace@proton.me>
2026-02-05 16:27:11 -0300

    Se corrgio para que pasaran los test

--- resources/js/pages/mensajes/componentes/vista-previa.tsx
+++
@@ -1,8 +0,0 @@
-// export default function VistaPrevia({ asunto, contenido }: { asunto: string, contenido: string }) {
-//     return (
-//         <div>
-//             <label>Asunto: {asunto}</label>
-//             <label>Contenido: {contenido}</label>
-//         </div>
-//     );
-// }
